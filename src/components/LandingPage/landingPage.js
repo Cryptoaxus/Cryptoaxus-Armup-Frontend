f191ba9ff31f57a9cc170b9048214774bec7a25b
Shahzaibawan1 <shahzaib@softaxus.com>
2023-06-23 17:57:27 +0500

    CRYP-81: Header, Footer and Application layout

---
+++ src/components/LandingPage/landingPage.js
@@ -0,0 +1,29 @@
+import React from "react";
+import "./landingpage.css"
+const LandingPage = () => {
+    return (
+        <div className="container">
+            <div >
+                <h1 className="Heading">Explore, Collect, and Sell  NFTs</h1>
+                <h1 className="Heading">Explore, Collect, and Sell  NFTs</h1>
+                <h1 className="Heading">Explore, Collect, and Sell  NFTs</h1>
+                <h1 className="Heading">Explore, Collect, and Sell  NFTs</h1>
+                <h1 className="Heading">Explore, Collect, and Sell  NFTs</h1>
+                <h1 className="Heading">Explore, Collect, and Sell  NFTs</h1>
+                <h1 className="Heading">Explore, Collect, and Sell  NFTs</h1>
+                <h1 className="Heading">Explore, Collect, and Sell  NFTs</h1>
+                <h1 className="Heading">Explore, Collect, and Sell  NFTs</h1>
+                <h1 className="Heading">Explore, Collect, and Sell  NFTs</h1>
+                <h1 className="Heading">Explore, Collect, and Sell  NFTs</h1>
+                <h1 className="Heading">Explore, Collect, and Sell  NFTs</h1>
+                <h1 className="Heading">Explore, Collect, and Sell  NFTs</h1>
+                <h1 className="Heading">Explore, Collect, and Sell  NFTs</h1>
+                <h1 className="Heading">Explore, Collect, and Sell  NFTs</h1>
+                <h1 className="Heading">Explore, Collect, and Sell  NFTs</h1>
+                <h1 className="Heading">Explore, Collect, and Sell  NFTs</h1>
+                <h1 className="Heading">Explore, Collect, and Sell  NFTs</h1>
+            </div>
+        </div>
+    );
+}
+export default LandingPage;
